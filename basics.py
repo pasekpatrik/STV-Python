d7c71625b83953c67da51fe9596f3a29b4ae1405
pasekpatrik <142804751+pasekpatrik@users.noreply.github.com>
2024-12-11 17:48:24 +0100

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,48 @@
+# Základní datové typy!
+# name: str = 'Patrik'
+# num: int = 12
+# float_num: float = 1.12
+# bol: bool = True # True / False
+# nekonecno: float = float('inf') # Nekonečno
+
+# list_num: list = [2, 3, 6]
+# list_str: list = ['Patrik', 'Petr', 'Anna']
+# list_num_str: list = ['Anna', 15, 'Evžen', 2]
+# list_all: list = ['Anna', 15, 2.23, True, []]
+# test_list: list = [1, 2, 3]
+
+# test_list.append(4) # Přišpedlí na konec pole
+# test_list.append(6)
+# test_list.append([1, 2])
+# test_list.pop() # Dá poslední prvek pryč
+# test_list.pop()
+
+# test_list.pop(1) # Dá prvek pryč podle indexu
+
+# print(list_str[1]) # Přístup k jednotlivým prvkům v listu (pole)
+# print(list_str[0])
+# print(list_str[2])
+
+# for i in range(3):
+#     print(list_str[i])
+
+# for i in range(len(list_str)):
+#     print(list_str[i])
+
+# for item in list_str:
+#     print(item)
+
+# index = 1
+# # Vypíše nám Zdravím tě 4 krát.
+# while index < 5: # Dokud je podmínka pravdívá (True), tak cyklus běží dál
+#     print(f'Zdravím tě po {index}')
+#     index = index + 1
+
+# for i in range(len(list_str) - 1, -1, -1):
+#     print(list_str[i])
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+# for i in range(10, -1, -1):
+#     print(i)
